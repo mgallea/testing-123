31849d9199b0e1ce8457f71cba7e56e563400d8e
Matthew Gallea <mattgallea@gmail.com>
2019-04-08 17:55:21 -0400

    Update tests

--- test/my_test.py
+++ test/my_test.py
@@ -2,4 +2,4 @@ from app.my_script import enlarge # load the `enlarge()` function to avoid NameE
 
 def test_enlarge(): # note the function name is prefixed with "test_"
     result = enlarge(3) # directly invoke the function we want to test
-    assert result == 300 # describe expectations for desired behavior
+    assert result == 3000000 # describe expectations for desired behavior
